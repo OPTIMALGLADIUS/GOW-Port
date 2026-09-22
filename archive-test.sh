72833ce0cf776a1f8f7eb3c686b518da76422b0a
OPTIMALGLADIUS <87300828+OPTIMALGLADIUS@users.noreply.github.com>
2026-09-22 12:35:09 +0530

    Fix extraction command formatting in archive-test.sh
    Removed space before output directory in extraction command.

--- archive-test.sh
+++ archive-test.sh
@@ -2,6 +2,6 @@ wget -O 7zip.tar.xz https://github.com/ip7z/7zip/releases/download/26.03/7z2603-
 tar -xf 7zip.tar.xz
 chmod +x 7zz
 mkdir -p pc-files
-./7zz x God-Of-War-AnkerGames.zip -o $PWD/pc-files
+./7zz x God-Of-War-AnkerGames.zip -o$PWD/pc-files
 ls
 ls pc-files
