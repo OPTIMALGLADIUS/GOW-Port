57e0ac10f04160b52e481ae37068a7a434180c83
OPTIMALGLADIUS <87300828+OPTIMALGLADIUS@users.noreply.github.com>
2026-09-22 10:51:42 +0530

    Fix wget command by removing output option

--- archive-test.sh
+++ archive-test.sh
@@ -1,5 +1,4 @@
-sudo apt install 7zip 7zip-standalone wget -y
-wget -O God-Of-War-AnkerGames.zip https://tunnel5.dlproxy.uk/download/nGw5A-kxTG7-bukYw_8crGI1HlQXlFBbv0G_WmaaMie07kpcS1elJWJO2oZzKcGk4Kd1kM9oDTN4mRhEB48qEyqT1RlW6TUBWnCNlhBsQV9AXiOrV02uiL15bAQFuA-uX0zHaqb2YLlvUHzytWTJ92_jRihuE5lJJbuir1Kz63S0_hE2NDFETLBg5W7oTGYLkFi0KWxK3TPtemtA-mJsivJEe-avMImHCjXc0_AzNnltTZYu0leUloGThXiDi_yT4s4P3TIjWuqqgf7FZ_m2F-TY05QLyMeHZhkZXHwe4crG4lDgSG2wQ8b-QqaezUDzNTULsodIcY-9K4ASZp-H6wgsrn0KcfELycEKyfNCqgnjpz5b9sz0wUrUTio-IULkbksOGTT0JJTGm7KHN-20vg?sig=sPBX1Kk4agWQAMdFQb2uH2D4_btHyfNZcND1xkpyEpY
+sudo apt install 7zip 7zip-standalone -y
 file God-Of-War-AnkerGames.zip
 xxd -l 64 God-Of-War-AnkerGames.zip
 7z l -slt God-Of-War-AnkerGames.zip
